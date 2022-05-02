022e213e27f60c6278fa06869bf90881c0aadc53
Ricardo Antonio Cutz Hernández <60110538+ricardocutzh@users.noreply.github.com>
2022-05-02 13:22:41 -0600

    Update
    Changes in line 41

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> changing text.
+            Edit <code>src/App.js</code> updated text
           </p>
           <LambdaDemo />
         </header>
